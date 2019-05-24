71b13233993c40e272e5a144a09454c13cc148da
KYIV\Dmytro_Yerusalimtsev <dmytro_yerusalimtsev@epam.com>
2019-05-24 18:54:44 +0300

    Updated stream processing.

--- importer/application/src/main/scala/com/rocketta/importer/speedometer/SpeedometerProcessor.scala
+++ importer/application/src/main/scala/com/rocketta/importer/speedometer/SpeedometerProcessor.scala
@@ -1,17 +1,17 @@
 package com.rocketta.importer.speedometer
 
-import com.rocketta.importer.core.{Sink, WithSinkFor}
+import com.rocketta.importer.core.Sink
 import com.rocketta.importer.kafkastreams.KafkaStreamsProcessor
 import org.apache.kafka.streams.StreamsBuilder
 import org.apache.kafka.streams.kstream.KStream
 
-final class SpeedometerProcessor(sink: Sink[Speed]) extends KafkaStreamsProcessor[Speed] with WithSinkFor[Speed] {
+final class SpeedometerProcessor(sink: Sink[Speed]) extends KafkaStreamsProcessor[Speed] {
   protected val appId: String = "speedometer-processor"
   private[this] val topic: String = "speedometer-telemetry"
 
   override protected def process(builder: StreamsBuilder): KStream[String, Speed] = {
     val speed = builder.stream[String, Speed](topic)
 
-    speed.foreach(st => writeTo(sink))
+    speed.foreach(st => save(st))
   }
 }
--- importer/core/src/main/scala/com/rocketta/importer/core/WithSinkFor.scala
+++
@@ -1,8 +0,0 @@
-package com.rocketta.importer.core
-
-trait WithSinkFor[M <: Message] {
-  this: Sink[M] =>
-  {
-    def writeTo(message: M): Unit = save(message)
-  }
-}
