916914315da04bc7c3a0d6b059e3b961b3873cee
KYIV\Dmytro_Yerusalimtsev <dmytro_yerusalimtsev@epam.com>
2019-05-27 16:22:03 +0300

    Implemented test sink

---
+++ importer/application/src/main/scala/com/rocketta/importer/Application.scala
@@ -0,0 +1,10 @@
+package com.rocketta.importer
+
+import com.rocketta.importer.graph.TestSink
+import com.rocketta.importer.speedometer.SpeedometerProcessor
+
+object Application extends App {
+  val speedProcessor = new SpeedometerProcessor with TestSink
+
+  speedProcessor.start()
+}
